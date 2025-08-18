0aa4bc2e96d208db48f75c0513d268beeb4a743c
kadiliimand <kadi.liimand@datanor.ee>
2025-08-18 10:19:30 +0300

    Initial commit

---
+++ src/main/java/com/liimand/bettinggameserver/BettingGameServerApplication.java
@@ -0,0 +1,13 @@
+package com.liimand.bettinggameserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BettingGameServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BettingGameServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liimand/bettinggameserver/BettingGameServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liimand.bettinggameserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BettingGameServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
